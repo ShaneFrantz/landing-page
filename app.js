4465769f52337f63685e90300e187eaa606d7f1b
Shane Frantz <shanesfrantz@gmail.com>
2022-02-20 23:00:18 -0600

    changed app.js

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found')
-        }
+var server = http.createServer(function(req, res) {
+    console.log('Request Made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    readStream.pipe(res);
+});
 
-        else {
-            response.write(data);
-        }
-
-
-    response.end();
-    });
-}
-
-http.createServer(onRequest).listen(8000);
+server.listen(3000, '127.0.0.1');
+console.log('Listening to port 3000');
